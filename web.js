7240689268403f93ab93f983b152e1953129c3a5
pranade <pranade@gmail.com>
2013-08-04 08:09:13 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -4,10 +4,14 @@ var htmlfile = "index.html";
 
 var app = express.CreateServer(express.logger());
 
-app.get('/'. function(request, response) {
+app.get('/', function(request, response) {
 	var html = fs.readFileSync(htmlfile).toString();
 	response.send(html);
 });
+
+app.use("/css",express.static(__dirname + '/css/'));
+app.use("/js",express.static(__dirname + '/js/'));
+app.use("/libs",express.static(__dirname + '/libs/'));
 
 var port = process.env.PORT||8080;
 app.listen(port, function(){
