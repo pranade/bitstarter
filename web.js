3ad706c2021c479226157794d327d4902783c538
Pratap Ranade <pranade@Prataps-MacBook-Air.local>
2013-08-04 04:17:22 -0400

    udpated web.js

--- web.js
+++ web.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
 
-var app = express.CreateServer(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var html = fs.readFileSync(htmlfile).toString();
-	response.send(html);
+var html = fs.readFileSync("index.html").toString();
+response.send(html);
 });
 
 app.use("/css",express.static(__dirname + '/css/'));
 app.use("/js",express.static(__dirname + '/js/'));
 app.use("/libs",express.static(__dirname + '/libs/'));
 
-var port = process.env.PORT||8080;
-app.listen(port, function(){
-	console.log("Listening on " + port);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-
